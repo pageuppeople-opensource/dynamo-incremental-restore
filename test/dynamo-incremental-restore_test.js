2a86eaeea9866d72984943be082dbf5813c41606
Abhaya Chauhan <abschauhan@gmail.com>
2016-03-29 22:48:55 +1100

    Original Record tests

--- test/dynamo-incremental-restore_test.js
+++ test/dynamo-incremental-restore_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var mockery = require('mockery');
-var assert = require('should');
+var should = require('should');
 var sinon = require('sinon');
 
 describe('DynamoDb Incremental backups restore', function() {
@@ -34,7 +34,7 @@ describe('DynamoDb Incremental backups restore', function() {
         before(function() {
             promise = dynamoIncrementalRestore();
         });
-        
+
         it('Should execute three documents', function(done) {
             promise
                 .then(function(data) {
@@ -64,7 +64,62 @@ describe('DynamoDb Incremental backups restore', function() {
 
     describe('Point in time restore', function() {
 
-        it('Should execute ')
+        describe('Deleted Record', function() {
+            it('Should delete \'deletedRecord\' row after it was deleted', function(done) {
+                var pointInTime = new Date("2016-03-29T23:56:55.000Z");
+                dynamoIncrementalRestore(pointInTime)
+                    .then(function(data) {
+                        data.should.have.properties('deletedRecord');
+                        data['deletedRecord'].deletedMarker.should.be.true;
+                        done();
+                    })
+                    .catch(function(err) {
+                        done(err);
+                    });
+            });
+
+            it('Should create \'deletedRecord\' row after it was created', function(done) {
+                var pointInTime = new Date("2016-03-28T23:56:40.000Z");
+                dynamoIncrementalRestore(pointInTime)
+                    .then(function(data) {
+                        data.should.have.properties('deletedRecord');
+                        should.not.exist(data['deletedRecord'].deletedMarker);
+                        done();
+                    })
+                    .catch(function(err) {
+                        done(err);
+                    });
+            });
+        });
+
+        describe('Original Record', function() {
+            it('Should delete original record before it existed', function(done) {
+                var pointInTime = new Date("2016-03-20T23:51:02.000Z");
+                dynamoIncrementalRestore(pointInTime)
+                    .then(function(data) {
+                        data.should.have.properties('originalRecord');
+                        data['originalRecord'].deletedMarker.should.be.true;
+                        done();
+                    })
+                    .catch(function(err) {
+                        done(err);
+                    });
+            });
+
+            it('Should create \'originalRecord\' row after it was created', function(done) {
+                var pointInTime = new Date("2016-04-01T23:51:02.000Z");
+                dynamoIncrementalRestore(pointInTime)
+                    .then(function(data) {
+                        data.should.have.properties('originalRecord');
+                        should.not.exist(data['originalRecord'].deletedMarker);
+                        done();
+                    })
+                    .catch(function(err) {
+                        done(err);
+                    });
+            });
+        });
+
 
     });
 
