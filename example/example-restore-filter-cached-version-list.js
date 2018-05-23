14d2e5297d739e41e0a8ebdff558402a12f4fa6f
AbhayaC <abhayac@pageuppeople.com>
2018-05-23 12:21:51 +1000

    Processing more async

--- example/example-restore-filter-cached-version-list.js
+++ example/example-restore-filter-cached-version-list.js
@@ -12,6 +12,7 @@ var dynamoIncrementalRestore = require('../');
 
 console.log(dynamoIncrementalRestore.restoreFilteredCachedVersionList({
     filterCachedVersionList: 'Formsmith.Green.Form.fout',
+    Bucket: "pageup-dynamodb-backups-au", //The bucket where the data is stored. e.g. pageup-dynamo-backup
     DestinationTableName: "Formsmith.DR.Form" , //The table to restore to
     region: "ap-southeast-2" //Dynamo Region. Change for each datacenter
 }));
